9168817da9305307cb1d182d764b64dac31e283b
Andrey <megainvest@yandex.ru>
2022-10-05 12:02:56 +0300

    Добавили условие в тело цикла

--- Program.cs
+++ Program.cs
@@ -5,3 +5,10 @@ M = stroka.Split(' ');
 var result = new string[M .Length];
 var realSize = 0;
 foreach (var value in M)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
