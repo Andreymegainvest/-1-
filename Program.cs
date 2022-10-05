a1d2309e9dbc003b6371b9472bf4b50167b37f5e
Andrey <megainvest@yandex.ru>
2022-10-05 12:01:25 +0300

    Цикл с перебором элементов массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿string[] M;
 Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+M = stroka.Split(' ');
+var result = new string[M .Length];
+var realSize = 0;
+foreach (var value in M)
